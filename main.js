e9138d184c18ab769a956de6d4f95b6b3119b2cb
jonathanfane <jonathanfane@hotmail.com>
2017-06-26 15:29:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello world - VERSION 1\n");
+  response.end("Hello World - Version 1\n");
 });
 
 // listen on localhost:8000
