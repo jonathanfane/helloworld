00c3a9cf8f86681c18a02e3ea844be455cd1aff8
jonathanfane <jonathanfane@hotmail.com>
2017-06-27 13:53:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain", "Cache-Control":"no-cache"});
-  response.end("Hello World - BLUE\n");
+  response.end("Hello World - GREEN\n");
 });
 
 // listen on localhost:8000
